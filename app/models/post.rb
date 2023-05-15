633ae585c4202807a9e60316c3ae2daf9f891051
petersamson-kingori <peters.kingori@student.moringaschool.com>
2023-05-15 14:25:16 +0300

    commit tree f400b14feda9e5f22f8e16817600626ec2566400

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validate :clickbait_title
+
+  private
+
+  def clickbait_title
+    unless title.present? && title.match?(/(Won't Believe|Secret|Top \d+|Guess)/)
+      errors.add(:title, "should be clickbait-y")
+    end
+  end
 end
